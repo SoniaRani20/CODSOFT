2d18ea61e73da72a0c48f86e3c42da0d5d91c5b4
SoniaRani20 <141821747+SoniaRani20@users.noreply.github.com>
2023-09-13 23:17:14 +0500

    Add files via upload

---
+++ task 1 codsoft/task1.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    $('#menu').click(function(){
+        $(this).toggleclass('fa-times');
+        $('header').toggleclass('toggle');
+    });
+    $(window).on('scroll load',function(){
+        $('#menu').removeclass('fa-times');
+        $('header').removeclass('toggle');
+
+        if( $(window).scrollTop() > 0){
+            $('.top').show();
+        }
+        else{
+            $('.top').hide();
+        }
+    });
+
+    $('a[href*="#"]').on('click',function(e){
+     e.preventDefault();
+     $('html, body').animate({
+   scrollTop : $($(this).attr('href')).offset().top,
+}, 
+500,
+'linear'
+);
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollButton = document.getElementById("scroll-up-button");
+
+    
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 300) {
+            scrollButton.classList.add("show");
+        } else {
+            scrollButton.classList.remove("show");
+        }
+    });
+
+    
+    scrollButton.addEventListener("click", function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    });
+});
